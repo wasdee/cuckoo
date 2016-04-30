a2e1c5de9883384df9ef43915f7fc8ddec4b5cbf
Thunpisit Amnuaikiatloet <fluke_family@hotmail.com>
2016-04-30 16:48:25 +0700

    Add: SparkTime lib for Particle

---
+++ Particle - Arduino/SparkTImeExample.ino
@@ -0,0 +1,52 @@
+#include "SparkTime/SparkTime.h"
+
+UDP UDPClient;
+SparkTime rtc;
+
+unsigned long currentTime;
+unsigned long lastTime = 0UL;
+String timeStr;
+
+void setup() {
+  rtc.begin(&UDPClient, "north-america.pool.ntp.org");
+  rtc.setTimeZone(-5); // gmt offset
+  Serial.begin(9600);
+}
+
+void loop() {
+    currentTime = rtc.now();
+    if (currentTime != lastTime) {
+      byte sec = rtc.second(currentTime);
+      if (sec == 10) {
+	// Build Date String
+	timeStr = "";
+	timeStr += rtc.dayOfWeekString(currentTime);
+	timeStr += ", ";
+	timeStr += rtc.monthNameString(currentTime);
+	timeStr += " ";
+	timeStr += rtc.dayString(currentTime);
+	timeStr += ", ";
+	timeStr += rtc.yearString(currentTime);
+	Serial.println(timeStr);
+      } else if (sec == 40) {
+	// Including current timezone
+	Serial.println(rtc.ISODateString(currentTime));
+      } else if (sec == 50) {
+	// UTC or Zulu time
+	Serial.println(rtc.ISODateUTCString(currentTime));
+      } else {
+	// Just the time in 12 hour format
+	timeStr = "";
+	timeStr += rtc.hour12String(currentTime);
+	timeStr += ":";
+	timeStr += rtc.minuteString(currentTime);
+	timeStr += ":";
+	timeStr += rtc.secondString(currentTime);
+	timeStr += " ";
+	timeStr += rtc.AMPMString(currentTime);
+	Serial.println(timeStr);
+      }
+      lastTime = currentTime;
+    }
+
+}
